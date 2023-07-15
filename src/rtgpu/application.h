9ddc68d37e7a3687636d7a46dc280129cf68fedd
Jackson Kaplan <JwyattK@gmail.com>
2023-07-15 13:08:24 -0700

    some app work

--- src/rtgpu/application.h
+++ src/rtgpu/application.h
@@ -12,9 +12,37 @@
 #ifndef RTGPU_APPLICATION_H_
 #define RTGPU_APPLICATION_H_
 
+#include "webgpu/webgpu_cpp.h"
+
 namespace rtgpu {
 
-class Application {};
+class Application {
+public:
+  Application()  = default;
+  ~Application() = default;
+
+  /* non-copyable */
+  Application(const Application &)            = delete;
+  Application &operator=(const Application &) = delete;
+
+  /* non-movable */
+  Application(Application &&)            = delete;
+  Application &operator=(Application &&) = delete;
+
+  void         Initialize();
+
+private:
+  /* Global WGPU objects */
+  wgpu::Instance instance_;
+  wgpu::Surface  surface_;
+  wgpu::Adapter  adapter_;
+  wgpu::Device   device_;
+
+  /* Global GLFW objects */
+
+  int width_  = 0;
+  int height_ = 0;
+};
 
 } // namespace rtgpu
 
