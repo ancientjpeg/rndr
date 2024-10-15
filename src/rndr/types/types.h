e4e8e2b863b7333c711592c66f32c55232524c53
Jackson Kaplan <JwyattK@gmail.com>
2024-10-14 19:16:12 -0700

    add error type

--- src/rndr/types/types.h
+++ src/rndr/types/types.h
@@ -19,6 +19,27 @@
 #include <webgpu/webgpu_cpp.h>
 
 namespace rndr {
+/********* Error Type **********/
+template <typename T>
+struct is_string
+    : public std::disjunction<std::is_same<char *, std::decay_t<T>>,
+                              std::is_same<const char *, std::decay_t<T>>,
+                              std::is_same<std::string, std::decay_t<T>>> {};
+
+struct Error {
+
+  Error(std::string message) : message_(message)
+  {
+  }
+
+  operator const char *() const
+  {
+    return message_.data();
+  }
+
+private:
+  std::string message_;
+};
 
 /********** Mesh Data **********/
 struct MeshData {
