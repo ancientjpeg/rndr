6df6bd07d67c5f44237e736777c148bd65cabbf1
Jackson Kaplan <JwyattK@gmail.com>
2024-11-02 13:17:33 -0700

    test tests lol

--- tests/sanity/application.tests.cpp
+++ tests/sanity/application.tests.cpp
@@ -6,6 +6,7 @@ TEST_CASE("Application initializes and destructs without fault")
 {
   auto         app         = std::make_unique<rndr::Application>();
   rndr::Result init_result = app->initialize();
+  REQUIRE(false);
   CHECK(init_result.ok());
   CHECK(app->isInitialized());
   app = nullptr;
