1132230cb851b3555e4575e04a109c458d1ec989
Jackson Kaplan <JwyattK@gmail.com>
2024-11-07 20:53:38 -0800

    add pipeline base file

---
+++ src/rndr/resources/pipeline_base.h
@@ -0,0 +1,22 @@
+/**
+ * @file pipeline_base.h
+ * @author Jackson Kaplan (jackson@minimal.audio)
+ * @date 2024-11-07
+ * Copyright © 2024 Minimal. All rights reserved.
+ */
+
+#ifndef RNDR_PIPELINE_BASE_H_
+#define RNDR_PIPELINE_BASE_H_
+
+namespace rndr {
+
+enum class PipelinePriority { Static, PerFrame, PerObject };
+
+class PipelineBase {
+public:
+private:
+};
+
+} // namespace rndr
+
+#endif
